177ed099a2ea15f5ad4412b046e697ca52b8356f
krckumar <Krishna@s.local>
2015-08-04 17:30:10 +0530

    Properties file changes

--- MobileFirstStarter/adapters/CatalogAdapter/CatalogAdapter-impl.js
+++ MobileFirstStarter/adapters/CatalogAdapter/CatalogAdapter-impl.js
@@ -5,23 +5,33 @@
  *  disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
  */
 
-function getCatalog() {
-	var path = 'boxstore-catalog.mybluemix.net';
-	
-	var input = {
-	    method : 'post',
-	    returnedContentType : 'xml',
-	    path : '/MFPSampleWebService/services/ProductLookup', 
-	    body : {
-            content : 
-            	'<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">\n' +
-                '<soap:Body>\n' + 
-                '</soap:Body>\n' +
-                '</soap:Envelope>\n',
-            contentType : 'text/xml; charset=utf-8'
-	    } 
-	};
+/**
+ *  WL.Server.invokeHttp(parameters) accepts the following json object as an argument:
+ *  
+ *  {
+ *  	// Mandatory 
+ *  	method : 'get' , 'post', 'delete' , 'put' or 'head' 
+ *  	path: value,
+ *  	
+ *  	// Optional 
+ *  	returnedContentType: any known mime-type or one of "json", "css", "csv", "plain", "xml", "html"  
+ *  	returnedContentEncoding : 'encoding', 
+ *  	parameters: {name1: value1, ... }, 
+ *  	headers: {name1: value1, ... }, 
+ *  	cookies: {name1: value1, ... }, 
+ *  	body: { 
+ *  		contentType: 'text/xml; charset=utf-8' or similar value, 
+ *  		content: stringValue 
+ *  	}, 
+ *  	transformation: { 
+ *  		type: 'default', or 'xslFile', 
+ *  		xslFile: fileName 
+ *  	} 
+ *  } 
+ */
 
-	input['headers'] = { 'SOAPAction' : '' };
-	return WL.Server.invokeHttp(input);
-}
+function getCatalog() {
+	var jsonData = [{"productID":"00001","title":"iPad Air 2","store":"bangalore","price":"600","photo":"/images/iPadAir2.jpg"},{"productID":"00002","title":"Xbox360 500GB","store":"Raleigh","price":"250","photo":"/images/xbox360.jpg"},{"productID":"00003","title":"Samsung 65-inch LED TV","store":"New York","price":"2600","photo":"/images/samsung65tv.jpg"},{"productID":"00004","title":"MacBook Pro","store":"Los Angeles","price":"1400","photo":"/images/macbook_pro.jpg"},{"productID":"00005","title":"Galaxy S6 Edge","store":"Boston","price":"300","photo":"/images/gs6edge.png"}];
+	return {getAllProductsDetailsReturn : jsonData};
+} 
+
